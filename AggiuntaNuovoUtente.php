94be38ac40c82ab91edeca95efb17861a64ffbb9
Alessandra Rota <35770400+alerota@users.noreply.github.com>
2018-05-22 15:33:36 +0200

    insertUtente non funzionante

--- AggiuntaNuovoUtente.php
+++ AggiuntaNuovoUtente.php
@@ -1,4 +1,3 @@
-<html>
 <?php
 $host = "localhost";
 $user = "root";
@@ -19,7 +18,7 @@ if(isset($_POST['submit'])) {
     if($result->num_rows == 1)
         header("Location: AggiuntaNuovoUtente.php?alert=Username gia' esistente");
     else {
-        $query2 = "INSERT INTO utenti(username, password) VALUES ('$username', '$password')";
+        $query2 = "INSERT INTO utenti(username, password) VALUES ('". $username ."', '" . $password ."')";
         $result = $connessione->query($query);
         if($result != null)
             header("Location: utenti.php?messaggio=Utente inserito con successo!");
@@ -27,53 +26,46 @@ if(isset($_POST['submit'])) {
             header("Location: utenti.php?alert=Errore nell'inserimento!");
     }
 }
-else {
+
 include 'menu.php';
 ?>
+<html>
 <body id="body">
-<div class="container py-5">
-    <div class="card">
-        <?php
-        if(isset($_GET['alert'])) {
-            ?>
-            <div class="alert alert-warning">
-                <strong>Warning! </strong> <?php echo $_GET['alert']; ?>
-            </div>
+    <div class="container py-5">
+        <div class="card">
             <?php
-        }
-        ?>
-        <div class="card-body">
+            if(isset($_GET['alert'])) {
+                ?>
+                <div class="alert alert-warning">
+                    <strong>Warning! </strong> <?php echo $_GET['alert']; ?>
+                </div>
+                <?php
+            }
+            ?>
             <h1 class="card-title text-center">Inserisci nuovo utente</h1>
-            <div class="container">
-                <form class="form-horizontal" role="form" id="form" action="" method="post">
-                    <div class="col-lg-10 col-lg-offset-0">
-                        <div class="form-group">
-                            <label for="firstName" class="col-sm-3 control-label">Username</label>
-                            <div class="col-sm-9">
-                                <input type="text" name="username" placeholder="Username"
-                                       class="form-control" autofocus>
-                            </div>
-                        </div>
-                        <div class="form-group">
-                            <label for="password" class="col-sm-3 control-label">Password</label>
-                            <div class="col-sm-9">
-                                <input type="password" name="password" placeholder="Password"
-                                       class="form-control">
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-sm-9 col-sm-offset-3">
-                                <input type="submit" value="Inserisci">
-                            </div>
+            <form action="AggiuntaNuovoUtente.php" method="POST" class="form-horizontal">
+                <div class="col-lg-10 col-lg-offset-0">
+                    <div class="form-group">
+                        <label for="username" class="col-sm-3 control-label">Username</label>
+                        <div class="col-sm-9">
+                            <input type="text" class="form-control" name="username">
                         </div>
                     </div>
-                </form>
-            </div>
+                    <div class="form-group">
+                        <label for="password" class="col-sm-3 control-label">Password</label>
+                        <div class="col-sm-9">
+                            <input type="password" class="form-control" name="password">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <div class="col-sm-9 col-sm-offset-3">
+                            <input id="submit" name="submit" type="submit" value="Inserisci" class="btn btn-primary" >
+                        </div>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
-</div>
 </body>
 </html>
-<?php
-}
