e6b36f7f4a222bb2a50551beeb9cc687321c28cc
DavidePizzoliCoso <39436715+DavidePizzoliCoso@users.noreply.github.com>
2018-05-22 09:17:58 +0200

    Modifiche ai form, modifiche al calendario, prime modifiche al check
    Ora i form sono adibiti alla modifica.
    Il calendario è stato migliorato, adesso è necessario trovare un template omogeneo per la pagina.
    Inizio delle modifiche al check con stili di bootstrap.
    Bozza della pagina homepage lato admin (nessun link ancora inserito).

--- AggiuntaNuovaFasciaOraria.php
+++ AggiuntaNuovaFasciaOraria.php
@@ -1,7 +1,50 @@
 <html>
+	<?php
+
+		// Connessione al DB
+
+		$host = "localhost";
+		$user = "root";
+		$pass = "";
+		$dbname = "ristorante";
+		$connessione = mysqli_connect($host, $user, $pass);
+		$db_selected=mysqli_select_db($connessione, $dbname);
+		
+		if($_GET != null)
+		{
+			$idImportante = $_GET["id"];
+			$sql = "select * from gestionefasceorarie where id_fascia = " . $idImportante . ";";
+
+			$result = mysqli_query($connessione, $sql);
+			$num_result = mysqli_num_rows($result);
+			
+			if($num_result == 1)
+			{
+				$row = mysqli_fetch_array($result);
+				$name = $row["nome"];
+				
+				$sql = "select * from fasceorarie where id_fascia = " . $idImportante . " order by orario;";
+				
+				$result = mysqli_query($connessione, $sql);
+				$num_result = mysqli_num_rows($result);
+				
+				$orari = array();
+				
+				for($i=0; $i < 6; $i++)
+					$orari[$i] = array();
+				
+				for($i=0; $i < $num_result; $i++)
+				{
+					$row = mysqli_fetch_array($result);
+					array_push($orari[$row["fase"]], $row["orario"]);
+				}
+			}
+			else
+				echo "<script>alert('Problemi durante il caricamento della fascia, chiedere assistenza...');</script>";
+		}
+	?>
 	<head>   
         <!-- Bootstrap CSS --><!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
@@ -39,12 +82,16 @@
 									<label>Nome identificativo</label>
 								</a>
 								<label></label>
-								<input type="text" name="nomeFascia" class="form-control" placeholder="Nome">
+								<input type="text" name="nomeFascia" class="form-control" 
+								<?php if(isset($name)) echo "value='" . $name . "'"; ?>
+								placeholder="Nome">
 								<span class="help-block">Questo nome verrà visualizzato solo nella parte amministrativa.</span>
 							</div>
 						</fieldset>
 						<hr>
-						<button onclick="vai();" class="btn btn-danger center-block" type="button" style="width: 100%; ">Aggiungi fascia oraria</button>
+						<button onclick="vai();" class="btn btn-danger center-block" type="button" style="width: 100%; ">
+						<?php if($_GET != null) echo "Aggiorna"; else echo "Aggiungi"; ?>
+						</button>
 					</div>
 					<div class="col-md-9">
 						<legend>Selezione orari</legend>
@@ -57,7 +104,13 @@
 									for($i=0; $i < $num / 3; $i++)
 									{
 										echo '<label>' . $fasi[$i] . '</label>
-										<div id="Fase' . $i . '" class="row"><br>
+										<div id="Fase' . $i . '" class="row">';
+										
+										if(isset($orari[$i]))
+											for($j = 0; $j < sizeof($orari[$i]); $j++)
+												echo '<input type="text" name="orario' . $i . '[]" class="form-control" value="' . $orari[$i][$j] . '" placeholder="Orario"><span class="help-block"></span>';
+										
+										echo '<br>
 											<button onclick="aggiungiOrario(\'Fase' . $i . '\');" class="btn btn-primary" type="button" >
 												Aggiungi riga
 											</button>
@@ -73,7 +126,13 @@
 									for($i= $num / 3; $i < $num * 2 / 3; $i++)
 									{
 										echo '<label>' . $fasi[$i] . '</label>
-										<div id="Fase' . $i . '"><br>
+										<div id="Fase' . $i . '">';
+										
+										if(isset($orari[$i]))
+											for($j = 0; $j < sizeof($orari[$i]); $j++)
+												echo '<input type="text" name="orario' . $i . '[]" class="form-control" value="' . $orari[$i][$j] . '" placeholder="Orario"><span class="help-block"></span>';
+										
+										echo '<br>
 											<button onclick="aggiungiOrario(\'Fase' . $i . '\');" class="btn btn-primary" type="button" >
 												Aggiungi riga
 											</button>
@@ -89,7 +148,13 @@
 									for($i= $num * 2 / 3; $i < $num; $i++)
 									{
 										echo '<label>' . $fasi[$i] . '</label>
-										<div id="Fase' . $i . '"><br>
+										<div id="Fase' . $i . '">';
+										
+										if(isset($orari[$i]))
+											for($j = 0; $j < sizeof($orari[$i]); $j++)
+												echo '<input type="text" name="orario' . $i . '[]" class="form-control" value="' . $orari[$i][$j] . '" placeholder="Orario"><span class="help-block"></span>';
+										
+										echo '<br>
 											<button onclick="aggiungiOrario(\'Fase' . $i . '\');" class="btn btn-primary" type="button" >
 												Aggiungi riga
 											</button>
--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <html>
 	<head>
         <link href="style/calendar.css" type="text/css" rel="stylesheet" />
+        <link href="vendor\bootstrap\css\bootstrap.min.css" type="text/css" rel="stylesheet" />
+        <link href="vendor\bootstrap\css\bootstrap.css" type="text/css" rel="stylesheet" />
         <!-- Required meta tags -->
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -8,26 +10,31 @@
         <!-- Bootstrap CSS -->
 
 	</head>
-	<body>
+	<body style="background-position:center; background-size: cover; background-image: url('');">
 		<?php
         include 'menu.php';
-		?>
-		<div class="container-fluid">
+		
+		if($_COOKIE != null)
+			include "Homepage.php";
+		else
+		{
+			echo '<div class="container-fluid">
 			<div class="row">
 				<div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><br></div>
 				<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12">
-					<?php
+					<br>';
 					include 'calendar.php';
 					$calendar = new Calendar();
 					echo $calendar->show();
-					?>
-				</div>
-				<div class="col-lg-6 col-md-12 col-sm-12 col-xs-12">
-					<?php
+				echo '</div>
+				<div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><br></div>
+				<div class="col-lg-1 col-md-1 col-sm-12 col-xs-12"><br></div>
+				<div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">';
 					if(isset($_GET["date"]))
 						include 'check.php';
-					?>
-				</div>
-			</div>
+				echo '</div>
+			</div>';
+		}
+		?>
 	</body>
 </html>    
