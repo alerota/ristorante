086178defd813d42ccf8d46c32040fad82525c74
Alessandra Rota <35770400+alerota@users.noreply.github.com>
2018-06-02 18:01:48 +0200

    Delete stagioni

--- elenchi/stagioni_giorniSpeciali.php
+++ elenchi/stagioni_giorniSpeciali.php
@@ -39,9 +39,9 @@ include '../menu.php';
 </head>
 
 <body id="body">
-<div class="container py-5">
-    <div class="card" id="wrapper">
-        <div class="card-body">
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
             <?php
             if(isset($_GET['messaggio'])) {
                 ?>
@@ -62,154 +62,146 @@ include '../menu.php';
             else {*/
             ?>
 
-            <div class="row">
-                <div class="col-sm-7">
+            <h1 class="card-title text-center">Lista stagioni <a href="AggiuntaNuovaStagione.php" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span></a></h1>
+            <br>
+            <table id="table">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Giorno inizio</th>
+                    <th>Giorno fine</th>
+                    <th>Priorità</th>
+                    <th>Edit</th>
+                    <th>Canc</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
 
-                    <h1 class="card-title text-center">Lista stagioni <a href="AggiuntaNuovaStagione.php" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span></a></h1>
-                    <br><br>
-                    <table id="table">
-                        <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Giorno inizio</th>
-                            <th>Giorno fine</th>
-                            <th>Priorità</th>
-                            <th>Edit</th>
-                            <th>Canc</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        <?php
+                $query = "SELECT * FROM stagioni WHERE giorno_inizio <> giorno_fine ORDER BY priorita DESC";
 
-                        $query = "SELECT * FROM stagioni WHERE giorno_inizio <> giorno_fine ORDER BY priorita DESC";
+                $result = $connessione->query($query);
+                $numrows = $result->num_rows;
 
-                        $result = $connessione->query($query);
-                        $numrows = $result->num_rows;
-
-                        if ($numrows) {
-							$nascondi1 = false;
-                            while ($row = $result->fetch_assoc()) {
-                                ?>
-                                <tr>
-                                    <?php
-                                    echo "<td>" . $row['nome_stagione'] . "</td>";
-
-                                    if(strcmp(substr($row['giorno_inizio'], 0, 1), "x") == 0) //se l'anno è x
-                                        $i = date("Y") . substr($row['giorno_inizio'], 1); //sostituisco x con l'anno corrente
-                                    else
-                                        $i = $row['giorno_inizio'];
-
-                                    $inizio = new DateTime($i);
-                                    echo "<td>" . $inizio->format('d-m-Y') . "</td>";
-
-                                    if(strcmp(substr($row['giorno_fine'], 0, 1), "x") == 0) //se l'anno è x
-                                        $f = date("Y") . substr($row['giorno_fine'], 1); //sostituisco x con l'anno corrente
-                                    else
-                                        $f = $row['giorno_fine'];
-
-                                    $fine = new DateTime($f);
-                                    echo "<td>" . $fine->format('d-m-Y') . "</td>";
-
-                                    echo "<td>" . $row['priorita'] . "</td>";
-                                    ?>
-                                    <td>
-
-                                        <a href="../forms/AggiuntaNuovaStagione.php?id=<?php echo $row['id_stagione'] ?>">
-                                            <span class="glyphicon glyphicon-pencil"></span>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a onclick="document.getElementById('idSupporto').value=<?php echo $row['id_prenotazione']; ?>;" class="confirm">
-                                            <span class="glyphicon glyphicon-trash"></span>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php
-                            }
-                        }
-						else
-							$nascondi1 = true;
+                if ($numrows) {
+                    $nascondi1 = false;
+                    while ($row = $result->fetch_assoc()) {
                         ?>
-                        </tbody>
-                    </table>
-					<?php
-					if($nascondi1)
-					{
-						echo "<script> document.getElementById('table').style.display = 'none'; </script>";
-						echo "<p style='text-align: center;'>Non sono presenti records.</p>";
-					}
-					?>
-                </div>
-
-
-                <div class="col-sm-5">
-                    <h1 class="card-title text-center">Lista giorni speciali <a href="AggiuntaGiornoSpeciale.php" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span></a></h1>
-                    <br><br>
-                    <table id="table1">
-                        <thead>
                         <tr>
-                            <th>Nome</th>
-                            <th>Giorno</th>
-                            <th>Edit</th>
-                            <th>Canc</th>
+                            <?php
+                            echo "<td>" . $row['nome_stagione'] . "</td>";
+
+                            if(strcmp(substr($row['giorno_inizio'], 0, 1), "x") == 0) //se l'anno è x
+                                $i = date("Y") . substr($row['giorno_inizio'], 1); //sostituisco x con l'anno corrente
+                            else
+                                $i = $row['giorno_inizio'];
+
+                            $inizio = new DateTime($i);
+                            echo "<td>" . $inizio->format('d-m-Y') . "</td>";
+
+                            if(strcmp(substr($row['giorno_fine'], 0, 1), "x") == 0) //se l'anno è x
+                                $f = date("Y") . substr($row['giorno_fine'], 1); //sostituisco x con l'anno corrente
+                            else
+                                $f = $row['giorno_fine'];
+
+                            $fine = new DateTime($f);
+                            echo "<td>" . $fine->format('d-m-Y') . "</td>";
+
+                            echo "<td>" . $row['priorita'] . "</td>";
+                            ?>
+                            <td>
+
+                                <a href="../forms/AggiuntaNuovaStagione.php?id=<?php echo $row['id_stagione'] ?>">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                </a>
+                            </td>
+                            <td>
+                                <a onclick="document.getElementById('idSupporto').value=<?php echo $row['id_stagione']; ?>;" class="confirm">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                </a>
+                            </td>
                         </tr>
-                        </thead>
-                        <tbody>
                         <?php
+                    }
+                }
+                else
+                    $nascondi1 = true;
+                ?>
+                </tbody>
+            </table>
+            <?php
+            if($nascondi1)
+            {
+                echo "<script> document.getElementById('table').style.display = 'none'; </script>";
+                echo "<p style='text-align: center;'>Non sono presenti records.</p>";
+            }
+            ?>
+            <h1 class="card-title text-center">Lista giorni speciali <a href="AggiuntaGiornoSpeciale.php" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-plus"></span></a></h1>
+            <br>
+            <table id="table1">
+                <thead>
+                <tr>
+                    <th>Nome</th>
+                    <th>Giorno</th>
+                    <th>Edit</th>
+                    <th>Canc</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
 
-                        $query = "SELECT * FROM stagioni WHERE giorno_inizio = giorno_fine ORDER BY priorita DESC";
+                $query = "SELECT * FROM stagioni WHERE giorno_inizio = giorno_fine ORDER BY priorita DESC";
 
-                        $result = $connessione->query($query);
-                        $numrows = $result->num_rows;
+                $result = $connessione->query($query);
+                $numrows = $result->num_rows;
 
-                        if ($numrows) {
-							$nascondi2 = false;
-                            while ($row = $result->fetch_assoc()) {
-                                ?>
-                                <tr>
-                                    <?php
-                                    echo "<td>" . $row['nome_stagione'] . "</td>";
-
-                                    if(strcmp(substr($row['giorno_inizio'], 0, 1), "x") == 0) //se l'anno è x
-                                        $i = date("Y") . substr($row['giorno_inizio'], 1); //sostituisco x con l'anno corrente
-                                    else
-                                        $i = $row['giorno_inizio'];
-
-                                    $inizio = new DateTime($i);
-                                    echo "<td>" . $inizio->format('d-m-Y') . "</td>";
-                                    ?>
-                                    <td>
-
-                                        <a href="../forms/AggiuntaGiornoSpeciale.php?id=<?php echo $row['id_stagione'] ?>">
-                                            <span class="glyphicon glyphicon-pencil"></span>
-                                        </a>
-                                    </td>
-                                    <td>
-                                        <a onclick="document.getElementById('idSupporto').value=<?php echo $row['id_prenotazione']; ?>;" class="confirm">
-                                            <span class="glyphicon glyphicon-trash"></span>
-                                        </a>
-                                    </td>
-                                </tr>
-                                <?php
-                            }
-                        }
-						else
-							$nascondi2 = true;
+                if ($numrows) {
+                    $nascondi2 = false;
+                    while ($row = $result->fetch_assoc()) {
                         ?>
-                        </tbody>
-                    </table>
-					<?php
-					if($nascondi2)
-					{
-						echo "<script> document.getElementById('table1').style.display = 'none'; </script>";
-						echo "<p style='text-align: center;'>Non sono presenti records.</p>";
-					}
-					?>
-                    <?php
-                    //}
-                    ?>
-                </div>
-            </div>
+                        <tr>
+                            <?php
+                            echo "<td>" . $row['nome_stagione'] . "</td>";
+
+                            if(strcmp(substr($row['giorno_inizio'], 0, 1), "x") == 0) //se l'anno è x
+                                $i = date("Y") . substr($row['giorno_inizio'], 1); //sostituisco x con l'anno corrente
+                            else
+                                $i = $row['giorno_inizio'];
+
+                            $inizio = new DateTime($i);
+                            echo "<td>" . $inizio->format('d-m-Y') . "</td>";
+                            ?>
+                            <td>
+
+                                <a href="../forms/AggiuntaGiornoSpeciale.php?id=<?php echo $row['id_stagione'] ?>">
+                                    <span class="glyphicon glyphicon-pencil"></span>
+                                </a>
+                            </td>
+                            <td>
+                                <a onclick="document.getElementById('idSupporto').value=<?php echo $row['id_stagione']; ?>;" class="confirm">
+                                    <span class="glyphicon glyphicon-trash"></span>
+                                </a>
+                            </td>
+                        </tr>
+                        <?php
+                    }
+                }
+                else
+                    $nascondi2 = true;
+                ?>
+                </tbody>
+            </table>
+            <input type="hidden" value="" id="idSupporto"/>
+            <?php
+            if($nascondi2)
+            {
+                echo "<script> document.getElementById('table1').style.display = 'none'; </script>";
+                echo "<p style='text-align: center;'>Non sono presenti records.</p>";
+            }
+            ?>
+            <?php
+            //}
+            ?>
         </div>
     </div>
 </div>
@@ -229,7 +221,7 @@ include '../menu.php';
             confirm: {
                 action: function () {
                     var a = document.getElementById("idSupporto").value;
-                    window.location.href = "prenotazioni.php?msg1=" + a;
+                    window.location.href = "../codici/deleteStagione.php?id=" + a;
                 },
                 text: 'Si',
                 btnClass: 'btn-danger',
